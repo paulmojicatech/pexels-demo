8f84ac708fdf159f6f1c9a5f672d88b2825db4c0
Paul Mojica <paulmojicatech@gmail.com>
2021-05-07 08:33:53 -0400

    fix compile error after removing interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SearchBarModule } from './shared-components/search-bar/search-bar.module';
 import { AppComponent } from './app.component';
-import { PexelsHttpInterceptor } from './services/http.interceptor';
 
 @NgModule({
   declarations: [
